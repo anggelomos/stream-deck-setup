981b04fd014a5472fcb84fc775b2600420651fd6
Angelo Mosquera <anggelomos@outlook.com>
2024-12-23 12:13:50 -0500

    Add dark mode and light mode commands

---
+++ scripts/enable-dark-mode.ps1
@@ -0,0 +1,2 @@
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0
---
+++ scripts/enable-light-mode.ps1
@@ -0,0 +1,2 @@
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 1
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 1
