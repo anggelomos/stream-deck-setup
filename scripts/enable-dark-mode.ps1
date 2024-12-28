fd2872fd5335cfb8c7bd926f447665d053b3208c
Angelo Mosquera <anggelomos@outlook.com>
2024-12-28 09:12:47 -0500

    Add comments and more commands to the dark mode scripts

--- scripts/enable-dark-mode.ps1
+++ scripts/enable-dark-mode.ps1
@@ -1,2 +1,10 @@
+# Enable dark mode
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0
+
+# Restart edge adding the flag to force dark mode
+Stop-Process -Name msedge
+Start-Process "msedge.exe" --enable-features=WebContentsForceDark
+
+# Restart windows explorer
+Stop-Process -Name explorer -Force
--- scripts/enable-light-mode.ps1
+++ scripts/enable-light-mode.ps1
@@ -1,2 +1,10 @@
+# Disable dark mode
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 1
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 1
+
+# Restart edge adding the flag to force dark mode
+Stop-Process -Name msedge
+Start-Process "msedge.exe" --disable-features=WebContentsForceDark
+
+# Restart windows explorer
+Stop-Process -Name explorer -Force
